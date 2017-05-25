21b7cb20f5b92e2ef1c7126e884d20aa6edb3186
anikredd <aniketh.info@gmail.com>
2017-05-25 00:16:21 -0400

    Initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,19 +2,40 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { HolidaysService } from './shared/services/holidays.service';
+import { HolidaysListComponent } from './holidays-list/holidays-list.component';
+import { HolidayDetailComponent } from './holiday-detail/holiday-detail.component';
+import { KeysPipe } from './shared/pipes/keys.pipe';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HolidaysListComponent,
+    HolidayDetailComponent,
+    KeysPipe
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    RouterModule.forRoot([
+      {
+        path: '',
+        redirectTo: '/holidays',
+        pathMatch: 'full'
+      },{
+        path: 'holidays',
+        component: HolidaysListComponent
+      },{
+        path: 'details/:holiday',
+        component: HolidayDetailComponent
+      }
+    ])
   ],
-  providers: [],
+  providers: [HolidaysService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
